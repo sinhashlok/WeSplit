34273c3fa3a01374d41d8409fd1fbefca435a8b2
Shlok Sinha <shlokjp@gmail.com>
2023-11-14 00:16:18 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shlok Sinha on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
